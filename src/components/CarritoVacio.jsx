bb38f4ea1b8dd0e0aa526b1621f4cf5b507b2ff9
Gerson Garayar <garayargerson20@gmail.com>
2023-03-20 10:12:25 -0500

    fix: arreglando componente carrito vacio

--- src/components/CarritoVacio.jsx
+++ src/components/CarritoVacio.jsx
@@ -2,8 +2,11 @@ import React from "react";
 
 export default function CarritoVacio() {
   return (
-    <div>
-      <h1>Tu carrito esta vacio</h1>
+    <div className="flex flex-col justify-center items-center">
+      <img className="w-96" src="carrito-vacio.jpg" alt="" />
+      <h1 className="text-slate-900 text-3xl font-bold">
+        Tu carrito esta vacio
+      </h1>
     </div>
   );
 }
